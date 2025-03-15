a3f861fe259b6d87ef89aef5f4c3f5234d47d976
SumitSinghRaghuwanshi1245 <sumittsinghr369@gmail.com>
2025-03-15 20:29:19 +0530

    first

---
+++ public/js/chessgame.js
@@ -0,0 +1,8 @@
+const socket = io();
+
+// socket.emit("BKL")                                 // YAHA SE DATA FRONTEND SE BACKEND PE JAAYEGA
+// socket.on("Sab BKL", function() {                   // YAHA SE DATA BACKEND SE FRONTEND PE AAYEGA
+    // console.log("duniya BKL")                        // YAHA BROWSER KE CONSOLE PE DIKHAYEGA
+// })
+
+
